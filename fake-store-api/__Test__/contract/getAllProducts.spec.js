65347aeb660a39ba6b7ab6e4576bd27a633d5851
Leonardo Mendes <mendes.leonardo@yahoo.com>
2025-09-14 21:40:09 -0300

    adding fake store api tests

---
+++ fake-store-api/__Test__/contract/getAllProducts.spec.js
@@ -0,0 +1,17 @@
+const { getRequest } = require('../../src/helpers/request')
+const { validateSchema } = require('../../src/helpers/schemaValidator')
+const schema = require('../../src/schema/products.schema')
+
+describe('Contract: Get All Products', () => {
+    let response
+    const path = '/products'
+
+    beforeAll(async() => {
+        response = await getRequest(path)
+    });
+
+    it('Validate Contract', () => {
+        validateSchema(response, schema)
+    });
+    
+});
